5f33cc8fc0742bc4457baf336b9d176a476dd0a3
AbdollahRaafat <abdollah.raafat.daif@gmail.com>
2024-03-18 11:29:55 +0200

    Fix 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,9 +17,12 @@ int main(void)
 	if (n < 0)
 	{
 		printf("%d is negative\n", n);
+	} else if (n > 0)
+	{
+		printf("%d is positive\n", n);
 	} else
 	{
-		printf("%d id positive\n", n);
+		printf("%d is zero\n", n);
 	}
 	return (0);
 }
