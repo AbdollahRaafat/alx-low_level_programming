735dbb42be4dead78f504a2252935dfae78af3e0
AbdollahRaafat <abdollah.raafat.daif@gmail.com>
2024-03-18 13:43:34 +0200

    Solve 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = '0';
+
+	while (x <= '9')
+	{
+		int y = '0';
+		while (y <= '9')
+		{
+			if (!(x == y || x > y))
+			{	
+				if (!(x == '0' && y == '1'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+		
+				putchar((char)x);
+				putchar((char)y);
+			}
+			y++;
+		}
+		x++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
