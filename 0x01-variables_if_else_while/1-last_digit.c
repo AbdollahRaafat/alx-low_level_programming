c9f8448b1400e420061ea6e2cc01f51ee180b8a3
AbdollahRaafat <abdollah.raafat.daif@gmail.com>
2024-03-18 11:52:28 +0200

    Solve linter style

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,8 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 	} else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and ", n, lastDigit);
+		printf("is less than 6 and not 0\n");
 	}
 	return (0);
 }
