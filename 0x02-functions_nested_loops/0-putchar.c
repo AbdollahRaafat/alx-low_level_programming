fef54adfc66475ad59378612145eba443dcf0857
AbdollahRaafat <abdollah.raafat.daif@gmail.com>
2024-03-19 11:52:29 +0200

    Solve 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char s[] = "_putchar";
+	int i;
+
+	for (i = 0; i < (int)sizeof(s) ; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
