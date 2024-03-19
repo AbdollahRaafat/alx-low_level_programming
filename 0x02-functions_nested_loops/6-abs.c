4243c9f4fe3d72598b979ed2c11f9cc71ed889a3
AbdollahRaafat <abdollah.raafat.daif@gmail.com>
2024-03-19 13:18:00 +0200

    Solve 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  _abs - retrun the abs value
+ * @n:number being passed around
+ * Return: positive either way
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
