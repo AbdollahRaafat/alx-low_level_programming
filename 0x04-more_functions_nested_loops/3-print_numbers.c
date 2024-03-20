ee54b8240c580aa213559e7314324391a3b1aee8
AbdollahRaafat <abdollah.raafat.daif@gmail.com>
2024-03-20 13:09:52 +0200

    Fix linter error'

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_number - print nums (0-9)
+ * print_numbers - print nums (0-9)
  * Return: void
  */
 
@@ -9,7 +9,7 @@ void print_numbers(void)
 {
 	int i;
 
-	for(i = '0'; i <= '9'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		_putchar(i);
 	}
