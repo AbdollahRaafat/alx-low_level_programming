66cba92785e834df946747210f546ef13c639596
AbdollahRaafat <abdollah.raafat.daif@gmail.com>
2024-03-20 14:28:21 +0200

    Solve linter error

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
+
 /**
- * print_diagonal - draws diagonal line whith \
+ * print_diagonal - draws diagonal line whith
  * @n: the length of line
  * Return: void
  */
@@ -9,14 +10,16 @@
 void print_diagonal(int n)
 {
 	int i;
-	
+
 	if (n <= 0)
 		_putchar('\n');
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		int x = i;
+
 		while (x-- > 0)
 			_putchar(' ');
+
 		_putchar('\\');
 		_putchar('\n');
 	}
