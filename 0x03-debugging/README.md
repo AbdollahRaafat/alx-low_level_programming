a44f131ae1aec6f7601de6769391741eb1080bc6
AbdollahRaafat <abdollah.raafat.daif@gmail.com>
2024-03-20 10:55:46 +0200

    Solve 0-main.c, main.h

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,9 @@
+## 0x03. C - Debugging
+
+These are the tasks of **C - Debugging** project from **ALX** program.
+
+
+- [x] 0. Multiple mains
+- [ ] 1. Like, comment, subscribe
+- [ ] 2. 0 > 972?
+- [ ] 3. Leap year
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * positive_or_negative - Entry point
+ * @n: int will be checked
+ * Return: void
+ */
+void positive_or_negative(int n)
+{
+
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	} else if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	} else
+	{
+		printf("%d is zero\n", n);
+	}
+}
