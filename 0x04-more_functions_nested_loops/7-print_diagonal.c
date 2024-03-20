911e26e4a2e442c583d1b8c663a3c8e575e69a87
AbdollahRaafat <abdollah.raafat.daif@gmail.com>
2024-03-20 14:24:30 +0200

    Solve 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws diagonal line whith \
+ * @n: the length of line
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	
+	if (n <= 0)
+		_putchar('\n');
+	for(i = 0; i < n; i++)
+	{
+		int x = i;
+		while (x-- > 0)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
