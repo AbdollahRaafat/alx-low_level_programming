6ed6a904daceee84d20a0c2de10c9a03b3574804
AbdollahRaafat <abdollah.raafat.daif@gmail.com>
2024-03-20 11:53:35 +0200

    Solve 3-print_remaining_days.c, main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,3 +1,5 @@
 int largest_number(int a, int b, int c);
 void positive_or_negative(int i);
 int max(int, int);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
