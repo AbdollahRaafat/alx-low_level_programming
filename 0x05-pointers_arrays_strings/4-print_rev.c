91b216c6b816061f58782e6dfc4687d9c94fce13
AbdollahRaafat <abdollah.raafat.daif@gmail.com>
2024-03-23 14:25:26 +0200

    Solve 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - print str in rev
+ * @str: string passed
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int n = strlen(s);
+	int i;
+
+	for (i = n - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
